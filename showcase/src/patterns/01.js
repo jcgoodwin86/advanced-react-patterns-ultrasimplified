4bfe9f6c4155ef9326257ceb98c388742fae549f
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2020-07-22 13:33:30 -0400

    switching comp

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -28,6 +28,49 @@ const withClapAnimation = (WrappedComponent) => {
         easing: mojs.easing.ease.out,
       });
 
+      const triangleBurst = new mojs.Burst({
+        parent: '#clap',
+        radius: {50: 95},
+        count: 5,
+        angle: 30,
+        children: {
+          shape: 'polygon',
+          radius: {6: 0},
+          stroke: 'rgba(211,54,0,0.5',
+          strokeWidth: 2,
+          angle: 210,
+          delay: 30,
+          speed: 0.2,
+          easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+          duration: tlDuration
+        }
+      })
+
+      const circleBurst = new mojs.Burst({
+        parent: '#clap',
+        radius: {50: 75},
+        count: 5,
+        angle: 35,
+        children:{
+          shape: 'circle',
+          fill: 'rgba(149,165,166,0.5)',
+          delay: 30,
+          speed: 0.2,
+          radius: {3: 0},
+          easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+        }
+      })
+      const countAnimation = new mojs.Html({
+        el: '#clapCount',
+        opacity: {0: 1},
+        y: {0: -30},
+        duration: tlDuration
+      }).then({
+        opacity: {1: 0},
+        y: -80,
+        delay: tlDuration/2
+      })
+
       const countTotalAnimation = new mojs.Html({
         el: '#ClapCountTotal',
         opacity: { 0: 1 },
@@ -42,6 +85,9 @@ const withClapAnimation = (WrappedComponent) => {
       const newAnimationTimeline = this.animationTimeline.add([
         scaleButton,
         countTotalAnimation,
+        countAnimation,
+        triangleBurst,
+        circleBurst
       ]);
       this.setState({ animationTimeline: newAnimationTimeline });
     }
@@ -103,7 +149,7 @@ const ClapIcon = ({ isClicked }) => {
 };
 
 const ClapCount = ({ count }) => {
-  return <span className={styles.count}>+ {count}</span>;
+  return <span id='clapCount' className={styles.count}>+ {count}</span>;
 };
 
 const CountTotal = ({ countTotal }) => {
